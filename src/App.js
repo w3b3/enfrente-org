18e5a7d545ff21d192e8d82f80d857fdfd4ebcc0
Daniel Santos <daniel.santos@datastax.com>
2023-01-12 20:28:59 -0400

    add card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import {CssBaseline} from "@mui/material";
+import {Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Paper, Typography} from "@mui/material";
 
 function App() {
     return (
-        <>
+        <Paper>
             <CssBaseline/>
-            <header className="App-header">
-                <h1>Enfrente.org</h1>
-            </header>
-            <main>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto aspernatur dolore est
-                    ipsum nisi nulla placeat recusandae, ullam. Accusantium at blanditiis doloremque error eveniet
-                    libero nihil porro sunt unde.</p>
-            </main>
-        </>
+            <Card sx={{maxWidth: 345}}>
+                <CardMedia
+                    aria-describedby={"https://unsplash.com/photos/xmSWVeGEnJw"}
+                    component="img"
+                    alt="image from Unsplash https://unsplash.com/photos/xmSWVeGEnJw"
+                    height="140"
+                    image="/unsplash.jpg"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Enfrente.org
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam aperiam autem beatae
+                        consequuntur delectus, dignissimos dolore fugiat ipsum maxime minima odit quidem quis recusandae
+                        rem, saepe soluta vero!
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </Paper>
     );
 }
 
